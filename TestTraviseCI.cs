6c4b9a7559843b3f93f988104c9ef9496422ddcf
A Egorov on work <mymailegorov@gmail.com>
2019-11-01 18:32:28 +0300

    commit

---
+++ TestTraviseCI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestTraviseCI
+{
+    public class TestTraviseCI
+    {
+		private string st = "TestTraviseCI";
+		public string St
+		{
+			get { return st; }
+			set { st = value; }
+		}
+	}
+}
